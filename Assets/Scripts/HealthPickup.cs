b69d62cef7e410991041c6999476e2cd3f5b2a8a
Xzhhhh414 <john.zh@outlook.com>
2024-03-29 17:04:14 +0800

    血条UI，BGM和音效

--- Assets/Scripts/HealthPickup.cs
+++ Assets/Scripts/HealthPickup.cs
@@ -7,6 +7,13 @@ public class HealthPickup : MonoBehaviour
     public int healthRestore = 20;
     public Vector3 spinRotationSpeed = new Vector3(0, 100, 0);
 
+    AudioSource pickupSource;
+
+    private void Awake()
+    {
+        pickupSource = GetComponent<AudioSource>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,12 +24,18 @@ public class HealthPickup : MonoBehaviour
     {
         Damageable damageable = collision.GetComponent<Damageable>();
 
-        if (damageable)
+        if (damageable && damageable.Health < damageable.MaxHealth)
         {
             bool wasHealed = damageable.Heal(healthRestore);
 
-            if(wasHealed)
+            if (wasHealed)
+            {
+                if (pickupSource)
+                    AudioSource.PlayClipAtPoint(pickupSource.clip, gameObject.transform.position, pickupSource.volume);
+
                 Destroy(gameObject);
+            }
+               
         }
     }
 
