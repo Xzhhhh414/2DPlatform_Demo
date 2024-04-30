d06ba77fa9e7e3adb331d8146d515c1f2aff23cf
殷琦 <yq101101@outlook.com>
2024-04-30 12:25:14 +0800

    子弹触地销毁

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -1,12 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Drawing;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
     public int damage = 10;
-    public Vector2 movespeed = new Vector2(3f,0);
+    public Vector2 movespeed = new Vector2(3f, 0);
     public Vector2 knockback = Vector2.zero;
 
     Rigidbody2D rb;
@@ -19,7 +20,7 @@ public class Projectile : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        rb.velocity = new Vector2(movespeed.x * transform.localScale.x,movespeed.y);
+        rb.velocity = new Vector2(movespeed.x * transform.localScale.x, movespeed.y);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -35,7 +36,12 @@ public class Projectile : MonoBehaviour
 
             if (gotHit)
                 Debug.Log(collision.name + "hit for" + damage);
-                Destroy(gameObject);
+            Destroy(gameObject);
         }
+        else if (collision.gameObject.layer == LayerMask.NameToLayer("Ground"))
+        {
+            Destroy(gameObject);
+        }
+
     }
 }
