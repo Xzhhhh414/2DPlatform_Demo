ff9d8a3a4affe974a6158ae6690e7696927b78d3
殷琦 <yq101101@outlook.com>
2024-05-06 18:55:56 +0800

    attack三连问题AI修复

--- Assets/Scripts/Utility/YQExtionsions.cs
+++ Assets/Scripts/Utility/YQExtionsions.cs
@@ -1,13 +1,33 @@
+using System.Threading;
 using System.Threading.Tasks;
 using UnityEngine;
 
 public static class YQExtionsions
 {
+    //public static async void SetTriggerByTime(this Animator animator, string triggerName, float time)
+    //{
+
+    //    animator.SetTrigger(triggerName);
+    //    await Task.Delay((int)(time * 1000));
+    //    animator.ResetTrigger(triggerName);
+    //}
+    private static CancellationTokenSource cts = new CancellationTokenSource();
+
     public static async void SetTriggerByTime(this Animator animator, string triggerName, float time)
     {
+        cts.Cancel(); // 取消前一个操作
+        cts = new CancellationTokenSource(); // 创建新的 CancellationTokenSource
 
         animator.SetTrigger(triggerName);
-        await Task.Delay((int)(time * 1000));
+        try
+        {
+            await Task.Delay((int)(time * 1000), cts.Token);
+        }
+        catch (TaskCanceledException)
+        {
+            // 如果任务被取消，就不需要重置触发器
+            return;
+        }
         animator.ResetTrigger(triggerName);
     }
 }
