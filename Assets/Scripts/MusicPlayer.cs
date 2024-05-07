b4b53a398435eea2bd0f536ab01d4d0af070d571
Xzhhhh414 <john.zh@outlook.com>
2024-05-07 17:01:19 +0800

    创角方法修改，配置可视化

--- Assets/Scripts/MusicPlayer.cs
+++ Assets/Scripts/MusicPlayer.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class MusicPlayer : MonoBehaviour
 {
-    public AudioSource introSource, loopSource;
+    //public AudioSource introSource;
+    public AudioSource loopSource;
 
     // Start is called before the first frame update
     void Start()
     {
-        introSource.Play();
-        loopSource.PlayScheduled(AudioSettings.dspTime + introSource.clip.length);
+        //introSource.Play();
+        //loopSource.PlayScheduled(AudioSettings.dspTime + introSource.clip.length);
 
-
+        loopSource.Play();
     }
 
     // Update is called once per frame
